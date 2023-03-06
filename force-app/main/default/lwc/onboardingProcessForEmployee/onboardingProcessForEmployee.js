39320f8a440a99d0b9cae687a820aba196b14cec
krishnaa12b <nagavamsikrishna88@gmail.com>
2023-03-06 14:54:34 +0530

    6th march

--- force-app/main/default/lwc/onboardingProcessForEmployee/onboardingProcessForEmployee.js
+++ force-app/main/default/lwc/onboardingProcessForEmployee/onboardingProcessForEmployee.js
@@ -139,8 +139,6 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
   isCompanyPolicies = false;
   isCompanyInformation = false;
   isConfirm = false;
-  showSpinner = false;
-
   correctImages = correctImage;
   wrongImages = wrongImage;
 
@@ -202,21 +200,23 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
   }
 
   connectedCallback() {
+    
+
 
     var dateVar = new Date();
     //Current Time 
-    this.currenttime = new Date().toLocaleTimeString();;
+    this.currenttime = new Date().toLocaleTimeString();
+
+     
 
 
     getUserContactInfo({ userId: this.userId }).then(result => {
-      this.contactID = result.Id;
+      console.log('getUserContactInfo------------->' ,result.contactDetails);
+      console.log('getUserContactInfo------------->' ,result.fileDetailsList);
+      this.contactID = result.contactDetails.Id;
 
-      this.UserName = result.Name;
-      //this.contactMaritalStatus = result.EMS_EM_Mstatus__c;
-      //console.log('this.contactMaritalStatus==>'+this.contactMaritalStatus);
-      //console.log('this.contactID==>'+this.recordId);
-      //console.log('this.contactID==>' + this.contactID);
-      const employye = result;
+      this.UserName = result.contactDetails.Name;
+      const employye = result.contactDetails;
       if (employye != null) {
         //this.readonlyfield=true;
 
@@ -252,33 +252,59 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
         this.VehicleNumber = employye.Vehicle_Number__c;
         if (this.Vehicletypeval != null) {
           this.showvehicle = true;
-          this.doyouhaveavehicleval = true;
+          this.checkboxValue = true;
         }
-
-        if (this.PostOnboardingConfirm) {
+        if (this.PostOnboardingConfirm == true) {
           this.buttonDisable = true;
           this.readonlyfield = true;
         }
+        if(this.padrressline1 === this.cadrressline1){
+          this.paFlag = true;
+          this.disableFlag = false;
+        }
 
-        if(this.pazip === this.cazip){
-          console.log('address Same')
-          this.inputcheckboxValue = event.target.checked;
-          console.log('address Same' +             this.inputcheckboxValue);
-    
-        }
-        else{
-          this.inputcheckboxValue = 'Unchecked';
-        }
+        result.fileDetailsList.forEach((currentItem) => {
+          console.log("currentItem.Title==============>>", currentItem.title);
+          if(currentItem.title.includes('Passport_')){
+          this.fileName = currentItem.title;
+          console.log('passport',this.fileName);
+          }
+         else if(currentItem.title.includes('DrivingLicense_')){
+          this.fileName1 = currentItem.title;
+            console.log('DrivingLicense_',this.fileName1);
+            }
+         else if(currentItem.title.includes('Form11_')){
+          this.fileName2 = currentItem.title;
+              console.log('Form11_',this.fileName2);
+         }
+         else if(currentItem.title.includes('Form2_')){
+          this.fileName3 = currentItem.title;
+          console.log('Form2_ photo',this.fileName3);
+         }
+         else if(currentItem.title.includes('Documents1_')){
+          this.fileName4 = currentItem.title;
+          console.log('Documents1 photo',this.fileName4);
+         }
+         else if(currentItem.title.includes('Documents2_')){
+          this.fileName5 = currentItem.title;
+          console.log('certificate photo',this.fileName5);
+         }
+         else if(currentItem.title.includes('Documents3_')){
+          this.fileName6 = currentItem.title;
+          this.ShowDocumentUpload = true;
+          this.hideDocumentUpload = false;
+          console.log('certificate photo',this.fileName6);
+         }
+         else if(currentItem.title.includes('Documents4_')){
+          this.fileName7 = currentItem.title;
+          console.log('certificate photo',this.fileName7);
+         }
+        })
 
         
 
         getPayrollInfo({ conId: this.contactID })
           .then(result => {
-
-            // this.payrollId = result[0].Id;
-            //console.log('Dataaaa-->' +JSON.stringify(result) );
-            //console.log('Dataaaa-->' +result[0].Beneficiary_Name__c );
-            // console.log('payroll result-->' + this.payrollId);
             const employye = result[0];
             if (employye != null) {
 
@@ -287,22 +313,14 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
               this.branchname = employye.Branch__c;
               this.accountNumber = employye.Beneficiary_Account_Number__c;
               this.iFSCRoutingNumber = employye.IFSC_Routing_Number__c;
-
             }
-
           }).catch(err => {
             //console.log('err----->',err);
-
           });
-
       }
-
     }).catch(err => {
       console.log(err);
-
     });
-
-
   }
 
   FirstName(event) {
@@ -400,7 +418,7 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
 
   @track cadrressline1;
   @track cadrressline2;
-  @track padrressline1 = '';
+  @track padrressline1;
   @track padrressline2;
   @track castate;
   @track cacity;
@@ -410,8 +428,7 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
   @track pazip;
   message;
   error;
-  addressId;
-
+  /*
   currentadrressline1(event) {
     this.cadrressline1 = event.target.value;
   }
@@ -452,6 +469,7 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
     this.pazip = event.target.value;
     //window.console.log(this.getAccountRecord.Name); 
   }
+  */
 
   inputcheckboxValue;
 
@@ -479,6 +497,146 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
 
     
   }
+  @track paFlag;
+  @track addressFlag;
+  @track disableFlag = true;
+
+    handleChange(event) {
+
+      this.addressFlag = true;
+          let addressList = this.template.querySelectorAll('.addressClass');
+          if (this.addressFlag) {
+            addressList.forEach((ele) => {
+              if (!ele.value) {
+                this.addressFlag = false;
+              }
+            });
+          }
+      this.disableFlag = this.addressFlag ? false : true;
+      if (!this.addressFlag) {
+        //console.log('1473');
+        this.paFlag = false;
+
+      }
+      const field = event.target.name;
+      if (field === 'EMS_EM_CAddress_Line_1__c') {
+          this.cadrressline1 = event.target.value;
+          //console.log('234',event.target.value);
+          if(event.target.value == ''){
+            //console.log('empty string');
+            this.cadrressline1 = null;
+
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.padrressline1 = this.cadrressline1;
+
+          }
+
+
+      }
+       if (field === 'EMS_EM_CAddress_Line_2__c') {
+          this.cadrressline2 = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.cadrressline2 = null;
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.padrressline2 = this.cadrressline2;
+          }
+
+
+      }
+        if (field === 'EMS_EM_CA_State__c') {
+          this.castate = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.castate = null;
+
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.pastate = this.castate;
+          }
+
+      }
+        if (field === 'EMS_EM_CA_City__c') {
+          this.cacity = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.cacity = null;
+
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.pacity = this.cacity;
+          }
+
+      }
+       if (field === 'EMS_EM_CA_Zip__c') {
+          this.cazip = event.target.value;
+          //console.log(event.target.value);
+
+          if(event.target.value == ''){
+            this.cazip = null;
+
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.pazip = this.cazip;
+          }
+
+      }
+      //permanent address
+      if (field === 'EMS_EM_PAddress_Line_1__c') {
+          this.padrressline1 = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.padrressline1 = null;
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.cadrressline1 = this.padrressline1;
+
+          }
+
+      }
+       if (field === 'EMS_EM_PAddress_Line_2__c') {
+          this.padrressline2 = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.padrressline2 = null;
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.cadrressline2 = this.padrressline2;
+          }
+      }
+        if (field === 'EMS_EM_PA_State__c') {
+          this.pastate = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.pastate = null;
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.castate = this.pastate;
+          }
+      }
+        if (field === 'EMS_EM_PA_City__c') {
+          this.pacity = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.pacity = null;
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.cacity = this.pacity;
+          }
+      }
+        if (field === 'EMS_EM_PA_Zip__c') {
+          this.pazip = event.target.value;
+          //console.log(event.target.value);
+          if(event.target.value == ''){
+            this.pazip = null;
+          }
+          if (this.inputcheckboxValue == 'Checked'){
+            this.cazip = this.pazip;
+          }
+      }
+    }
 
   // Identity Information"....
 
@@ -547,10 +705,12 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
     this.VehicleNumber = event.target.value;
   }
   doyouhaveavehicle(event) {
-    this.doyouhaveavehicleval = event.target.checked ? 'Checked' : 'Unchecked';
-
-    if (this.doyouhaveavehicleval == 'Checked') {
+      this.checkboxValue = event.target.checked;
+      //console.log('this.checkboxValue--------->'  +  this.checkboxValue)
+    if (this.checkboxValue == true) {
       this.showvehicle = true;
+      this.checkboxValue = true
+      //console.log('this.checkboxValue--------->'  +  this.checkboxValue)
     } else {
       this.showvehicle = false;
     }
@@ -1515,27 +1675,6 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
   @track totalFamilyRecords;
   @track dependenciesRecordsArray = [];
 
-  get getdependenciesRecordsArrayLength() {
-    return this.dependenciesRecordsArray.length === 5;
-  }
-  get showLabel() {
-    return this.dependenciesRecordsArray.length === 0 ? "Create" : "Edit";
-  }
-  get hideExsistingDependencies() {
-    return this.dependenciesRecordsArray.length === 0 ? false : true;
-  }
-  get checkForDependents() {
-    return this.dependenciesRecordsArray.length === 0 ? true : false;
-  }
-  get showMedicalDetails() {
-    return this.medicalInsuranceId ? true : false;
-  }
-  get showMedicalDetailsLabel() {
-    return this.medicalInsuranceId ? "Edit" : "Create";
-  }
-
-
-
   @wire(getFamilyInfo, { contactId: "$contactID" })
   getfamilyinformationrecords(totalRecords) {
     this.totalFamilyRecords = totalRecords;
@@ -1551,36 +1690,7 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
       //console.log("Error OCcured With", totalRecords.error);
     }
   }
-
-
-  keyIndex = 0;
-  @track itemList = [
-    {
-      id: 0
-    }
-  ];
-
-  addRow() {
-    //console.log("Length", this.itemList.length);
-    //console.log("Length", this.dependenciesRecordsArray);
-    if (this.dependenciesRecordsArray.length >= 5 || (this.dependenciesRecordsArray.length + this.itemList.length >= 5)) {
-      //console.log("I WILL NOT EXECUETE")
-    } else if (this.dependenciesRecordsArray.length <= 5 || (this.dependenciesRecordsArray.length + this.itemList.length <= 5)) {
-      //console.log("Executed")
-      ++this.keyIndex;
-      var newItem = [{ id: this.keyIndex }];
-      this.itemList = this.itemList.concat(newItem);
-    }
-  }
-
-  removeRow(event) {
-    if (this.itemList.length >= 2) {
-      this.itemList = this.itemList.filter(function (element) {
-        return parseInt(element.id) !== parseInt(event.target.accessKey);
-      });
-    }
-  }
-
+  isLoaded = false;
   contactPhone = false;
   memberDateOfBirth = false;
   Emergencycheckboxval;
@@ -1606,45 +1716,25 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
     }
 
   }
-  @track isLoading = false;
-
-  handleSubmit1(event) {
-    var isVal = true;
-    this.template.querySelectorAll('.inputValField').forEach(element => {
-      isVal = isVal && element.reportValidity();
-    });
-    if (isVal) {
-
-      this.template.querySelectorAll('.createDependenciesRecord').forEach(element => {
-        //console.log("element", JSON.stringify(element));
-        element.submit();
-        this.showSpinner = true;
-        this.updateRecordView();
-        this.dispatchEvent(
-          new ShowToastEvent({
-            title: 'Success',
-            message: 'Family record successfully created',
-            variant: 'success',
-          }),
-        );
-        
-        refreshApex(this.totalFamilyRecords);
-        this.showSpinner = false;
-        this.isFamilyInformationCheckbox = true;
-
+  
+  handlefamilySuccess(event){
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: 'Success',
+        message: 'Family Member Added successfully',
+        variant: 'success',
+      }),
+    );
+    
+    refreshApex(this.totalFamilyRecords);
+    const inputFields = this.template.querySelectorAll('lightning-input-field');
+    if (inputFields) {
+      inputFields.forEach(field => {
+          field.reset();
       });
+  } this.isLoaded = false;
+    this.isFamilyInformationCheckbox = true;
 
-    } else {
-      this.dispatchEvent(
-        new ShowToastEvent({
-          title: 'Error creating record',
-          message: 'Please enter all the required fields',
-          variant: 'error',
-        }),
-      ).finally(()=>{
-        this.handleIsLoading(false);
-    });
-    }
     if (this.isWelcomeaboardValueChecked === false || this.isPersonalUpdateCheckbox === false || this.isIdentifyDetailsCheckbox === false || this.isEmploymentDetailsCheckbox === false || this.isAddressDetailsCheckbox === false || this.isFamilyInformationCheckbox === false || this.isFinancialInformationCheckbox === false
       || this.isVehicleDetailsCheckbox === false || this.isPFFormsCheckbox === false || this.isDocumentsValueChecked === false || this.isCompanyPoliciesValueChecked === false || this.isCompanyInformationValueChecked === false) {
       this.buttonDisable = true;
@@ -1654,18 +1744,30 @@ export default class OnboardingProcessForEmployee extends NavigationMixin(Lightn
     }
   }
 
-  //show/hide spinner
-   handleIsLoading(isLoading) {
-    this.isLoading = isLoading;
-}
+  handlesubmitfamily(event){
+    
+    event.preventDefault();
+    const fields = event.detail.fields;
+    fields.Resource__c = this.contactID;
+    this.template.querySelector('lightning-record-edit-form').submit(fields);
+    this.isLoaded = true;
+  }
 
-updateRecordView() {
-   setTimeout(() => {
-        eval("$A.get('e.force:refreshView').fire();");
-   }, 1000); 
-}
+  handlefamilyerror(event){
+    
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: 'error',
+        message: event.detail.detail,
+        variant: 'error',
+      }),
+    );
+    this.isLoaded = false;
+  }
+
 
   deleteDependencyRecord(event) {
+    this.isLoaded = true;
     const recordId = event.target.dataset.recordid;
     //console.log('record id--------> ' + event.target.dataset.recordid);
     deleteRecord(recordId)
@@ -1673,11 +1775,12 @@ updateRecordView() {
         this.dispatchEvent(
           new ShowToastEvent({
             title: 'Success',
-            message: 'Record  Deleted Suscessfully',
+            message: 'Family Member Deleted Suscessfully',
             variant: 'success'
           })
         );
         refreshApex(this.totalFamilyRecords);
+        this.isLoaded = false;
         this.isFamilyInformationCheckbox = true;
         if (this.totalFamilyRecords.data.listfamilyrecords.length === 1) {
           this.dependenciesRecordsArray = [];
